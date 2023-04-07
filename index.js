be22a1c6ccce9a15e1ec6d8dcd4018de616294fb
Alekseev Sergei <nec13@mail.ru>
2023-04-07 20:07:30 +0300

    feat: tasks complete

--- index.js
+++ index.js
@@ -1,41 +1,158 @@
-const task1 = function (maxNumber) {
-  for (let i = 0; i <= maxNumber; i++) {
-    if (i === 0) console.log(`${i} - это ноль`);
-    else if (!(i % 2)) console.log(`${i} это четное число`);
-    else if (i % 2) console.log(`${i} это нечетное число`);
-  }
-};
-task1(10);
+const task1 = () => {
+  const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7
+  };
 
-const task2 = function () {
-  const array = [1, 2, 3, 4, 5, 6, 7];
-  array.splice(3, 2);
-  return array;
-};
-console.log(task2());
-
-const task3 = function () {
-  function generateArrayWithRandomNumbers(length) {
-    const array = [];
-    for (let i = 0; i < length; i++) {
-      array.push(Math.floor(Math.random() * 10));
+  //var 1
+  for (let key in numbers) {
+    if (numbers[key] >= 3) {
+      console.log(`${key} - ${numbers[key]}`);
     }
-    return array;
   }
 
-  const array = generateArrayWithRandomNumbers(5);
-  console.log(`Массив: ${array}
-  1) Сумма элементов массива: ${(array.reduce((acc, item) => acc + item), 0)}
-  2) Минимальное число в массиве: ${Math.min(...array)}
-  3) Есть ли в массиве число 3? ${array.some(item => item === 3) ? 'Да' : 'Нет'} `);
+  //var 2
+  console.log(Object.values(numbers).filter(value => value >= 3));
 };
+console.log('-----------------Task 1-------------------');
+task1();
+
+const task2 = () => {
+  const post = {
+    author: 'John', // вывести этот текст
+    postId: 23,
+    comments: [
+      {
+        userId: 10,
+        userName: 'Alex',
+        text: 'lorem ipsum',
+        rating: {
+          likes: 10,
+          dislikes: 2 // вывести это число
+        }
+      },
+      {
+        userId: 5, // вывести это число
+        userName: 'Jane',
+        text: 'lorem ipsum 2', // вывести этот текст
+        rating: {
+          likes: 3,
+          dislikes: 1
+        }
+      }
+    ]
+  };
+
+  //var 1
+  console.log(`
+  1) ${post.author};
+  2) ${post.comments[0].rating.dislikes};
+  3) ${post.comments[1].userId};
+  4) ${post.comments[1].text};
+  `);
+
+  //var2
+  const { author } = post;
+  const { dislikes } = post.comments[0].rating;
+  const { userId, text } = post.comments[1];
+  console.log(`
+  1) ${author};
+  2) ${dislikes};
+  3) ${userId};
+  4) ${text};
+  `);
+};
+console.log('-----------------Task 2-------------------');
+task2();
+
+const task3 = () => {
+  const products = [
+    {
+      id: 3,
+      price: 200
+    },
+    {
+      id: 4,
+      price: 900
+    },
+    {
+      id: 1,
+      price: 1000
+    }
+  ];
+
+  function sale(price, percentage) {
+    return price - price * (percentage / 100);
+  }
+
+  //var 1
+  // products.forEach(obj => (obj.price = obj.price * 0.85));
+
+  //var 2
+  products.forEach(obj => (obj.price = sale(obj.price, 15)));
+
+  for (let product of products) {
+    console.log(product);
+  }
+};
+console.log('-----------------Task 3-------------------');
 task3();
 
-const task4 = function (maxLength) {
-  let string = '';
-  for (let i = 0; i < maxLength; i++) {
-    string += 'x';
-    console.log(string);
-  }
+const task4 = () => {
+  const products = [
+    {
+      id: 3,
+      price: 127,
+      photos: ['1.jpg', '2.jpg']
+    },
+    {
+      id: 5,
+      price: 499,
+      photos: []
+    },
+    {
+      id: 10,
+      price: 26,
+      photos: ['3.jpg']
+    },
+    {
+      id: 8,
+      price: 78
+    }
+  ];
+
+  console.log(products.filter(product => product.photos && product.photos.length > 0));
+  console.log(products.sort((firstProduct, secondProduct) => firstProduct.price - secondProduct.price));
 };
-task4(20);
+console.log('-----------------Task 4-------------------');
+task4();
+
+const task5 = () => {
+  const en = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  const ru = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+  const daysOfTheWeek = {};
+  let daysOfTheWeek2 = {};
+
+  if (en.length === ru.length) {
+    //var 1
+    for (let i = 0; i < en.length; i++) {
+      daysOfTheWeek[en[i]] = ru[i];
+    }
+
+    //var2
+    daysOfTheWeek2 = en.reduce((acc, key, value) => {
+      acc[key] = ru[value];
+      return acc;
+    }, {});
+  }
+
+  console.log(daysOfTheWeek);
+  console.log(daysOfTheWeek2);
+};
+console.log('-----------------Task 5-------------------');
+task5();
