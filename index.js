260f5426cd13dc0d285af950c185b1c9f44a3e1d
Alekseev Sergei <nec13@mail.ru>
2023-05-25 21:15:35 +0300

    feat:

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function getEvenNumbers(array) {
-  return array.filter(number => number % 2 === 0);
+function createGreeting(name) {
+  return () => `Hello, ${name}`;
 }
-console.log(`Even ${getEvenNumbers([1, 2, 3, 2, 4, 1, 5])}`);
-
-function calculateAverage(array) {
-  return array.reduce((acc, number) => acc + number, 0) / array.length;
-}
-console.log(`Avg: ${calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9])}`);
-
-function capitalizeFirstLetter(string) {
-  return string
-    .split(' ')
-    .map(word => word.slice(0, 1).toUpperCase() + word.slice(1))
-    .join(' ');
-}
-console.log(`Capitalize: ${capitalizeFirstLetter('hello mate, how are you?')}`);
+const greeting = createGreeting('John');
+console.log(greeting());
