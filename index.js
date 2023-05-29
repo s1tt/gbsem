ffe05dfef9e5c18d4584cc11a13c1c37f1058a2a
Alekseev Sergei <nec13@mail.ru>
2023-05-30 01:09:41 +0300

    feat: ECMA. tasks of the 2nd workshop are complete

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
-function createGreeting(name) {
-  return () => `Hello, ${name}`;
+/*
+Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+Свойство title (название) - строка, название книги.
+Свойство author (автор) - строка, имя автора книги.
+Свойство pages (количество страниц) - число, количество страниц в книге.
+Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+*/
+
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+
+  displayInfo() {
+    console.log(`The book, titled "${this.title}" was written by author ${this.author}. There are ${this.pages} pages in this book`);
+  }
 }
-const greeting = createGreeting('John');
-console.log(greeting());
+const book1 = new Book('Title', 'Author', 2000);
+book1.displayInfo();
+/*
+Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+Свойство name (имя) - строка, имя студента.
+Свойство age (возраст) - число, возраст студента.
+Свойство grade (класс) - строка, класс, в котором учится студент.
+Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+javascript
+
+// Пример использования класса
+const student1 = new Student(""John Smith"", 16, ""10th grade"");
+student1.displayInfo();
+// Вывод:
+// Name: John Smith
+// Age: 16
+// Grade: 10th grade
+
+const student2 = new Student(""Jane Doe"", 17, ""11th grade"");
+student2.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Age: 17
+// Grade: 11th grade"
+*/
+
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+
+  displayInfo() {
+    console.log(`Student Name: ${this.name}. His age: ${this.age}. Student's grade: ${this.grade}`);
+  }
+}
+
+const student1 = new Student('John Smith', 16, '10th grade');
+student1.displayInfo();
+// Вывод:
+// Name: John Smith
+// Age: 16
+// Grade: 10th grade
+
+const student2 = new Student('Jane Doe', 17, '11th grade');
+student2.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Age: 17
+// Grade: 11th grade"
